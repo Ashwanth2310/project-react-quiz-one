30ce74f157f3229de6a887bc2b599044d67375e7
Ashwanth2310 <ashwanth@rajakumar.net>
2024-01-29 10:41:00 +0530

    updates

---
+++ quiz/src/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import HomeComponent from "./componenets/HomeComponent"
+import QuizComponent from "./componenets/QuizComponent"
+import ResultComponent from "./componenets/ResultComponent" 
+
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <HomeComponent/>
+        <QuizComponent/>
+        <ResultComponent/>
+      </div>
+    )
+  }
+}
